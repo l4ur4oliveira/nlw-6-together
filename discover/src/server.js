c28b39fca7d611c78443e68cacde888af547e0f5
Laura de Oliveira <l4ur4.88@gmail.com>
2021-06-24 19:30:23 -0300

    Discover: Aula 04 - Atualiza estrutura do projeto e add libs do Node e rotas

---
+++ discover/src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const route = require('./route');
+const path = require('path');
+
+const server = express();
+
+server.set('view engine', 'ejs');
+server.set('views', path.join(__dirname, 'views'));
+
+server.use(route);
+server.use(express.static('public'));
+
+server.listen(3000, () => console.log('Serve on port 3000...'));
