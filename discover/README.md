efc2bf18a19dc2a2ba261cb3546a03e463e1b8ed
Laura de Oliveira <l4ur4.88@gmail.com>
2021-06-25 00:51:36 -0300

    Discover: Aula 04 - Cria banco de dados e salva rooms

--- discover/README.md
+++ discover/README.md
@@ -1,3 +1,9 @@
 # Trilha Discover - Projeto Rocket.Q :speech_balloon:
 
 Aplicação de gerenciamento de perguntas e respostas, com criação de salas para que as pessoas possam tirar suas dúvidas mediante disponibilidade de senha.
+
+## Run
+
+- Instalar dependências `yarn install`
+- Criar bando de dados `yarn init-db`
+- Rodar o server `yarn start`
--- discover/src/controllers/RoomController.js
+++ discover/src/controllers/RoomController.js
@@ -1,6 +1,24 @@
+const Database = require('../db/config');
+
 module.exports = {
-  create(req, res) {
-    let roomId = 12345;
+  async create(req, res) {
+    const db = await Database();
+    const { password } = req.body;
+    let roomId = '';
+
+    for (let i = 0; i < 6; i++) {
+      roomId += Math.floor(Math.random() * 10).toString();
+    }
+
+    await db.run(`INSERT INTO rooms (
+      id,
+      pass
+    ) VALUES (
+      ${parseInt(roomId)},
+      ${password}
+    )`);
+
+    await db.close();
 
     res.redirect(`/room/${roomId}`);
   }
